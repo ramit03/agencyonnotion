ae81b865258dc619528e2b7a7ae74a1ff2c521de
Ramit <ramitsingh12@gmail.com>
2023-10-17 18:09:17 +0530

    small changes

--- app/(root)/thankyou/page.tsx
+++ app/(root)/thankyou/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 function Thankyou() {
   return (
-    <section className="flex lg:flex-row flex-col lg:py-48 md:py-32 py-28 w-full h-full lg:pl-44 lg:pr-20 lg:px-0 md:px-20 px-10 lg:gap-12 md:gap-16 gap-20">
+    <section className="flex lg:flex-row flex-col lg:py-48 md:py-32 py-28 w-full h-full lg:pl-44 lg:pr-20 lg:px-0 md:px-20 px-10 lg:gap-48 md:gap-16 gap-20">
       <div className="flex flex-col lg:gap-12 md:gap-10 gap-10 lg:w-2/3 lg:text-[32px] md:text-[28px] text-lg text-gray-1">
         <h1 className="header md:text-5xl text-[32px]">Just one more thing</h1>
         <p className="font-helvetica lg:leading-[47.10px] md:leading-[41.21px]">
@@ -30,10 +30,10 @@ function Thankyou() {
         <p>Thank you for registering, and we look forward to seeing you soon!</p>
         <p className="lg:text-2xl md:text-xl text-base font-bold lg:leading-9 md:leading-[29.44px] leading-normal">P.S. If your plans change and you can't attend, kindly opt out through the link in your email. This small step ensures a seamless experience for all. We appreciate your understanding!</p>
         <Link href={'https://chat.whatsapp.com/IhHepfIw8huGUHka71jLd9'}>
-          <Button className="bg-red-1 my-4 hover:bg-red-1/75 lg:w-1/2 md:w-2/3 w-4/5 lg:h-28 h-[60px]">
-            <div className="flex flex-row gap-2  items-center py-2">
+          <Button className="bg-red-1 my-4 hover:bg-red-1/75 lg:w-[60%] md:w-2/3 w-4/5 lg:h-28 h-[60px]">
+            <div className="flex flex-row gap-2 items-center py-2">
               <Image
-                className=" w-[38px] h-[43px]"
+                className=" w-[38px] h-[43px] md:w-[55px] md:h-[62px] lg:w-[69px] lg:h-[77px]"
                 src='/images/Whatsapp.png'
                 width={70}
                 height={77}
@@ -44,7 +44,7 @@ function Thankyou() {
           </Button>
         </Link>       
       </div>
-      <div className="flex lg:items-end md:items-center lg:w-1/3 md:w-full justify-center">
+      <div className="flex items-center lg:w-1/3 md:w-full justify-center">
         <Image
           className="md:w-[300px] h-[500px] lg:w-[525px] lg:h-[889px]"
           src="/images/message.svg"
