2bf52a8a975cf31c85462fdebc1c5c17c2ba3d92
Ramit <ramitsingh12@gmail.com>
2023-10-19 14:50:48 +0530

    font size changes

--- app/(root)/terms&conditions/page.tsx
+++ app/(root)/terms&conditions/page.tsx
@@ -7,10 +7,10 @@ function page() {
         Agency on Notion Terms and Conditions
       </h1>
       <p className="subheader_2 ">Last Updated: 19/10/2023</p>
-      <ol className="header_2 list-decimal space-y-6 ml-16 mb-10">
+      <ol className="header_2 list-decimal space-y-6 md:ml-16 ml-0 mb-10">
         <li>
           Registration
-          <ol className="subheader_2 list-decimal space-y-2 ml-6">
+          <ol className="subheader_2 list-decimal space-y-2 md:ml-6 ml-2">
             <li>
               <span className="font-bold">Eligibility: </span>Webinars are open
               to participants who meet the specified eligibility criteria stated
@@ -25,7 +25,7 @@ function page() {
         </li>
         <li>
           Intellectual Property
-          <ol className="subheader_2  space-y-2 list-decimal ml-6">
+          <ol className="subheader_2  space-y-2 list-decimal md:ml-6 ml-2">
             <li>
               <span className="font-bold">Ownership: </span>All content
               presented during the webinar, including but not limited to
@@ -42,7 +42,7 @@ function page() {
         </li>
         <li>
         Conduct
-          <ol className="subheader_2  space-y-2 list-decimal ml-6">
+          <ol className="subheader_2  space-y-2 list-decimal md:ml-6 ml-2">
             <li>
               <span className="font-bold">Professionalism: </span>Participants are expected to engage in respectful and professional behavior throughout the webinar.
             </li>
@@ -53,7 +53,7 @@ function page() {
         </li>
         <li>
         Limitation of Liability
-          <ol className="subheader_2  space-y-2 list-decimal ml-6">
+          <ol className="subheader_2  space-y-2 list-decimal md:ml-6 ml-2">
             <li>
               <span className="font-bold">No Guarantees: </span>Agency on Notion does not guarantee the accuracy, completeness, or usefulness of any information presented during the webinar.
             </li>
@@ -64,7 +64,7 @@ function page() {
         </li>
         <li>
         Privacy
-          <ol className="subheader_2 space-y-2 list-decimal ml-6">
+          <ol className="subheader_2 space-y-2 list-decimal md:ml-6 ml-2">
             <li>
               <span className="font-bold">Privacy Policy: </span>By participating in the webinar, you agree to the terms outlined in our Privacy Policy.
             </li>
@@ -72,7 +72,7 @@ function page() {
         </li>
         <li>
         Changes to Terms and Conditions
-          <ol className="subheader_2  space-y-2 list-decimal ml-6">
+          <ol className="subheader_2  space-y-2 list-decimal md:ml-6 ml-2">
             <li>
               <span className="font-bold">Modification: </span>Redendron Media reserves the right to update or modify these Terms and Conditions at any time without prior notice.
             </li>
@@ -80,7 +80,7 @@ function page() {
         </li>
         <li>
         Governing Law
-          <ol className="subheader_2  space-y-2 list-decimal ml-6">
+          <ol className="subheader_2  space-y-2 list-decimal md:ml-6 ml-2">
             <li>
               <span className="font-bold">Jurisdiction </span>These Terms and Conditions are governed by the laws of the Republic of India, without regard to its conflict of laws principles.
             </li>
@@ -88,7 +88,7 @@ function page() {
         </li>
         <li>
         Contact Us
-          <ol className="subheader_2  space-y-2 list-decimal ml-6">
+          <ol className="subheader_2  space-y-2 list-decimal md:ml-6 ml-2">
             <li>
             If you have any questions or concerns about this Privacy Policy, please
         contact us at{" "}
