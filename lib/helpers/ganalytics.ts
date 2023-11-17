cec514ffbf2b28bbcd26368543bd75f9889abd01
Ramit <ramitsingh12@gmail.com>
2023-11-17 17:09:02 +0530

    replay

--- lib/helpers/ganalytics.ts
+++ lib/helpers/ganalytics.ts
@@ -1,10 +1,12 @@
 import ReactGA from 'react-ga';
 
 export const initGa= () => {
+    console.log("Initializing Google Analytics");
     ReactGA.initialize('G-4XRZGC9133');
 };
 
 export const logPageView = () => {
+    console.log("Logging page view for: ", window.location.pathname);
     ReactGA.set({ page:window.location.pathname});
     ReactGA.pageview(window.location.pathname);
 }
